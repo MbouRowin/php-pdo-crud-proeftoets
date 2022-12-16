3ec5abb59e3eab398b0ff46615fcf94684d05ea4
Rowin <117640285+MbouRowin@users.noreply.github.com>
2022-12-16 11:28:43 +0100

    Add delete function

--- read.php
+++ read.php
@@ -10,6 +10,12 @@ $options = [
 
 $pdo = new PDO($dsn, "root", "", $options);
 
+if (isset($_GET["delete"])) {
+    $stmt = $pdo->prepare('DELETE FROM RichestPeople WHERE Id = :Id');
+    $stmt->bindValue(":Id", $_GET["delete"]);
+    $stmt->execute();
+}
+
 $people = $pdo->query('SELECT * FROM RichestPeople ORDER BY Networth DESC');
 
 ?>
@@ -45,7 +51,13 @@ $people = $pdo->query('SELECT * FROM RichestPeople ORDER BY Networth DESC');
                 <td><?= $row['Networth'] ?></td>
                 <td><?= $row['Age'] ?></td>
                 <td><?= $row['MyCompany'] ?></td>
-                <td>Delete</td>
+                <td>
+                    <a href="?delete=<?= $row['Id'] ?>">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="red" class="bi bi-x" viewBox="0 0 16 16">
+                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                        </svg>
+                    </a>
+                </td>
             </tr>
         <?php endwhile ?>
     </table>
